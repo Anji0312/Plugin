40c76e070992e7e0d056bfdffc1e5be0c191f0d6
Anji0312 <anjali03dec@gmail.com>
2020-01-16 19:33:22 +1000

    Update Subscribe.php

---
+++ Subscribe.php
@@ -0,0 +1,12 @@
+
+<?php
+/*
+Plugin Name: Subscribe Button
+Plugin URI: https://anjaliv.sgedu.site/wordpress03/
+Description: Display subcribe button and count
+Version: 1.0.0
+Author: Anjali Verma
+Author URI: https://anjaliv.sgedu.site/wordpress03/
+*/
+
+// Exit if accessed directly
